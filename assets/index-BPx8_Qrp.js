8d3b5072497697d050da0e1de584f3de72b42e22
soad <soad>
2025-09-29 22:09:16 +0300

    Updates

---
+++ assets/index-BPx8_Qrp.js
@@ -0,0 +1,1 @@
+import{l as s,m as t,n as e,q as a,E as o,F as r}from"./index-BRXUQCg1.js";import{_ as n}from"./HomeProducts.vue_vue_type_script_setup_true_lang-94Jwsgp-.js";const c={class:"pb-7"},d={__name:"index",setup(_){return(l,m)=>(t(),s(r,null,[e("div",c,[a(n)]),e("div",null,[a(o)])],64))}};export{d as default};
