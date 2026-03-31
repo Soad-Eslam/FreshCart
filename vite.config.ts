ebc487aa73dde1c90f5b5e6067874d8805f44c8c
Soad Eslam <114537882+Soad-Eslam@users.noreply.github.com>
2026-03-31 13:57:24 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/E-commerce_App/",
+  base:"/FreshCart/",
   plugins: [
     vue(),
     vueDevTools(),
